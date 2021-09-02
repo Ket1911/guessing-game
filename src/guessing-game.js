ab1d7af63adf8d89af63849e96e2b22053ab11c4
Katerina Vinskovska <kostiukkaterina@gmail.com>
2021-09-02 19:39:29 +0300

    init: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,25 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return  Math.round((this.max + this.min) / 2) 
     }
 
     lower() {
 
+        this.max = Math.round((this.max + this.min) / 2) ;
+        return this.max
+
     }
 
     greater() {
-
+        this.min = Math.round((this.max + this.min) / 2);
+        return this.min
     }
 }
 
-module.exports = GuessingGame;
+module.exports = GuessingGame
